80500f71fe88b6d0d87fd754e48a017bc9fbda88
Suresh Kaleyannan <hello@suresh.app>
2025-08-16 17:57:14 +0800

    Update ContactForm.tsx

--- src/components/Preact/ContactForm.tsx
+++ src/components/Preact/ContactForm.tsx
@@ -1,75 +1,13 @@
-import { useState, useRef } from 'preact/hooks'
-import emailjs from "@emailjs/browser"
-
-type StatusType = {
-    status: boolean,
-    message: string
-}
+// ContactForm.tsx
 
 const ContactForm = () => {
-    const [mailStatus, setMailStatus] = useState<StatusType>({ status: false, message: "" })
-    const [isLoading, setisLoading] = useState<boolean>(false)
-
-    const NameRef = useRef<HTMLInputElement>(null)
-    const EmailRef = useRef<HTMLInputElement>(null)
-    const MessageRef = useRef<HTMLTextAreaElement>(null)
-
-    const HandleFormSubmit = async (e: SubmitEvent) => {
-        e.preventDefault();
-
-        if (!NameRef.current || !EmailRef.current || !MessageRef.current) return
-
-        const name = NameRef.current.value
-        const email = EmailRef.current.value
-        const message = MessageRef.current.value
-
-        // Add your receiver email here
-        const receiverEmail = "receiver@example.com"
-
-        const templateParams = {
-            from_name: name,
-            from_email: email,
-            to_email: receiverEmail, // dynamic receiver
-            message: message,
-        }
-
-        try {
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-            if (!emailRegex.test(email)) {
-                throw new Error('🙄 Invalid Email ID!')
-            }
-
-            setisLoading(true)
-
-            // Direct values instead of import.meta.env
-            const mailRes = await emailjs.send(
-                "service_sureshksapp",     // your service ID
-                "template_sureshapp",      // your template ID
-                templateParams,
-                "JhUYWI_fdQ0bfdICH"        // your public key
-            )
-
-            if (mailRes.status !== 200) {
-                throw new Error("😵 Message not Sent")
-            }
-
-            setMailStatus({ status: true, message: "👍 Message Sent!" })
-            setisLoading(false)
-
-            NameRef.current.value = ""
-            EmailRef.current.value = ""
-            MessageRef.current.value = ""
-        } catch (error: { message: string } | any) {
-            setMailStatus({ status: false, message: error.message })
-        } finally {
-            setTimeout(() => {
-                setMailStatus({ status: false, message: "" })
-            }, 3000)
-        }
-    }
-
     return (
-        <form onSubmit={HandleFormSubmit} className="Fade_Up bg-LinkBtnGradient rounded-md w-full lg:max-w-[650px] px-4 py-2 outline outline-1 outline-white/20 flex_center flex-col">
+        <form
+            className="Fade_Up bg-LinkBtnGradient rounded-md w-full lg:max-w-[650px] px-4 py-4 outline outline-1 outline-white/20 flex_center flex-col"
+            action="https://hello.suresh.app/db/submit.php"
+            method="post"
+        >
+            {/* Name */}
             <label htmlFor="name" className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2">
                 Name
                 <input
@@ -80,9 +18,10 @@ const ContactForm = () => {
                     className="w-full p-2 mt-1 rounded-md border-none outline-none bg-background text-foreground"
                     autoComplete="name"
                     required
-                    ref={NameRef}
                 />
             </label>
+
+            {/* Email */}
             <label htmlFor="email" className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2">
                 Email
                 <input
@@ -93,9 +32,10 @@ const ContactForm = () => {
                     className="w-full p-2 mt-1 rounded-md border-none outline-none bg-background text-foreground"
                     autoComplete="email"
                     required
-                    ref={EmailRef}
                 />
             </label>
+
+            {/* Message */}
             <label htmlFor="message" className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2">
                 Message
                 <textarea
@@ -104,31 +44,30 @@ const ContactForm = () => {
                     name="message"
                     placeholder="Enter your Message"
                     className="w-full p-2 mt-1 rounded-md border-none outline-none bg-background text-foreground resize-none"
-                    ref={MessageRef}
+                    required
                 />
             </label>
 
-            <div className="w-full flex justify-start items-center gap-4">
+            {/* Captcha */}
+            <div className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2">
+                <label className="form-label" id="captcha">4 + 4 =</label>
+                <input
+                    className="form-control w-full p-2 mt-1 rounded-md border-none outline-none bg-background text-foreground"
+                    type="number"
+                    name="captcha-response"
+                    required
+                />
+                <input type="hidden" name="captcha-solution" value="8" />
+            </div>
+
+            {/* Submit */}
+            <div className="w-full flex justify-start items-center gap-4 mt-2">
                 <button
-                    className="flex_center gap-4 border-none bg-background text-foreground outline outline-1 outline-white/20 my-1 mx-2 py-2 px-6 lg:px-20 rounded-md"
+                    className="flex_center gap-4 border-none bg-background text-foreground outline outline-1 outline-white/20 my-1 mx-2 py-2 px-6 lg:px-20 rounded-md btn btn-primary"
                     type="submit"
-                    disabled={isLoading}
                 >
-                    {
-                        isLoading ? (
-                            <>
-                                <span>Sending</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-loader-2 animate-spin"><path d="M21 12a9 9 0 1 1-6.219-8.56" /></svg>
-                            </>
-                        ) : (
-                            <>
-                                <span>Submit</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send-horizontal"><path d="m3 3 3 9-3 9 19-9Z" /><path d="M6 12h16" /></svg>
-                            </>
-                        )
-                    }
+                    Send Now
                 </button>
-                <span>{mailStatus.message}</span>
             </div>
         </form>
     )
